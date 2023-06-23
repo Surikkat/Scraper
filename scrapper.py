a59b630c5ad2ce7c5af295bbae34f55a91b551ea
Surikkat <74894572+Surikkat@users.noreply.github.com>
2023-06-23 11:44:40 +0300

    imports and main

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,22 @@
 from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+import numpy as np
+from PyInquirer import prompt
+import os
+import signal
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except AssertionError:
+        print('Interface not supported, please use terminal or command prompt.')
 
 
